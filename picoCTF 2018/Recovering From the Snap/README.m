7ca635ae7f0e09f2c18c845e1db9659e30a48bca
Shrimant More <smshrimant@gmail.com>
2018-12-22 00:54:16 +0530

    Update README.m

--- picoCTF 2018/Recovering From the Snap/README.m
+++ picoCTF 2018/Recovering From the Snap/README.m
@@ -1,1 +1,3 @@
 Recovering From the Snap - 150 
+
+Lot of people had solved this challenge. I have also seen video writeups where the method used really hectic. So I thought to make it little bit easy.
